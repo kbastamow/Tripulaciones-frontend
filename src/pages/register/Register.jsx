35d680c23957944d5b5cb0d06867cd47802a651d
Jose Llanas <josellanasmota86@gmail.com>
2023-06-07 13:07:56 +0200

    register implemented

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { register } from "../../features/auth/authSlice";
+
+
+const Register = () => {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, surname, email, password } = formData;
+
+  const dispatch = useDispatch()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(register(formData))
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" value={name} onChange={onChange} placeholder="name" />
+      <input type="text" name="surname" value={surname} onChange={onChange} placeholder="surname"/>
+      <input type="email" name="email" value={email} onChange={onChange} placeholder="email"/>
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={onChange}
+        placeholder="password"
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
