ed804ccb3983d5245d2e42fed5b1045279e10d9b
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-06-07 19:51:32 +0200

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Register from './pages/register/Register';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -13,11 +13,19 @@ export const authSlice = createSlice({
 });
 
 export const register = createAsyncThunk("auth/register", async (user) => {
-    try {
-        return await authService.register(user);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    return await authService.register(user);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export const login = createAsyncThunk("auth/login", async (user) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export default authSlice.reducer;
