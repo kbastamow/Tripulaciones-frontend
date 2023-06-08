4550ce2c1cec8a4a258998a570f4a7239ccbe9ac
Jose Llanas <josellanasmota86@gmail.com>
2023-06-08 10:33:05 +0200

    problem with form antdesign

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { register } from "../../features/auth/authSlice";
-
+import { Button, Checkbox, Form, Input } from 'antd';
 
 const Register = () => {
 
@@ -16,32 +16,192 @@ const Register = () => {
 
   const dispatch = useDispatch()
 
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    const myData = {...values, email: values.email + '@edem.es'}
+    console.log(myData)
+    // dispatch(register(values))
+
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(register(formData))
+  // const onFinish = (e) => {
+  //   e.preventDefault();
+  //   dispatch(register(user))
+    // const myData = {...values, email: values.email + '@edem.es'}
+    // console.log(myData)
+  // };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
   };
+
+  // const onChange = (e) => {
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
+
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   dispatch(register())
+  // };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" name="name" value={name} onChange={onChange} placeholder="name" />
-      <input type="text" name="surname" value={surname} onChange={onChange} placeholder="surname"/>
-      <input type="email" name="email" value={email} onChange={onChange} placeholder="email"/>
-      <input
-        type="password"
+    <div>
+      <h1>Register</h1>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your name!',
+            },
+          ]}
+        >
+        <Input/>
+        </Form.Item>
+
+        <Form.Item
+          label="Surname"
+          name="surname"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your surname!',
+            },
+          ]}
+        >
+        <Input/>
+        </Form.Item>
+  
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+        <Input/>
+        <span>@edem.es</span>
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+        <Input.Password/>
+        </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+      </Button>
+    </Form.Item>
+  </Form>
+      {/* <Form onSubmit={onSubmit}
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Name"
+        name="name"
+        value={name}
+        onChange={onChange}
+        rules={[{ required: true, message: 'Please input your name!' }]}
+      >
+      
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Surname"
+        name="surname"
+        value={surname}
+        onChange={onChange}
+        rules={[{ required: true, message: 'Please input your surname!' }]}
+      >
+          <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        value={email}
+        onChange={onChange}
+        rules={[{ required: true, message: 'Please input your email!' }]}
+      >
+          <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
         name="password"
         value={password}
         onChange={onChange}
-        placeholder="password"
-      />
-      <button type="submit">Register</button>
-    </form>
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit" onSubmit={onSubmit}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form> */}
+  </div>
   );
 };
 
