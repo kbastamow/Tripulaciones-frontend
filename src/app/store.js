c7e0efcfb172046bdb6783219f9d2a9392ed2143
kbastamow <bastamowkat@gmail.com>
2023-06-09 15:56:59 +0200

    EventOne - axios

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import auth from '../features/auth/authSlice';
 import events from "../features/events/eventSlice"
 
+
 export const store = configureStore({
   reducer: {
     auth, 
-    events
+    events, 
   },
 })
