590b3544051da5d790ada76fe82c5e4772378a68
Jose Llanas <josellanasmota86@gmail.com>
2023-06-09 15:42:28 +0200

    updateProfile page implementing

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -8,15 +8,33 @@ const register = async (userData) => {
   return res.data;
 };
 
-const updateProfile = async (userData) => {
-  const res = await axios.put(API_URL + "/users/updateProfile", userData); 
+const login = async(userData)=>{
+  const res = await axios.post(API_URL + '/users/login',userData)
+  if (res.data) {
+  localStorage.setItem("user", JSON.stringify(res.data.user));
+  localStorage.setItem("token", JSON.stringify(res.data.token)); 
+ }
+  return res.data
+ }
+
+const updateProfile = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  console.log('token',token);
+  
+  const res = await axios.put(API_URL + "/users/updateProfile", {
+    
+    headers: {
+      authorization: token,
+      }
+  }); 
   console.log(res.data)  
   return res.data;
 };
 
 const authService = {
   register,
-  updateProfile
+  updateProfile,
+  login
 };
 
 export default authService;
