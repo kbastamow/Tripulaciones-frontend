dcfd3312f13c43d7f7102f5cea23970cc7af5371
kbastamow <bastamowkat@gmail.com>
2023-06-10 17:58:10 +0200

    Home and event details. Styles in progress

--- src/components/eventCard/EventCard.jsx
+++ src/components/eventCard/EventCard.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { Card } from 'antd';
-const { Meta } = Card;
 import { Button, Space, Divider } from 'antd';
 import "./EventCard.scss"
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import logo from "../../assets/logo.png"
 const imagePath = "http://localhost:8080/images/event/"
-
+const { Meta } = Card;
 
 const EventCard = () => {
 
@@ -25,19 +25,19 @@ if(events?.length <= 0){
 return (
 <>
       {events.map(event => (
-        <div key={event._id}>
+        <div key={event._id} className="eventcard-container">
         <Card
         hoverable
         className="eventcard-general"
-        cover={<img alt="evento" src={imagePath + event.image}/>}
+        cover={<img alt="evento" src={imagePath + event.image} onError={(e) => e.target.src = logo}/>}
           >
         <div>
-        <img src="{imagePath + event.image}" alt="" />
+        {/* <img src="{imagePath + event.image}" alt="" /> */}
         <h2>{event.title}</h2>
         <p className="card-text-event">{event.description}</p>
         </div>
 
-        <Space wrap className='center-content-div'> 
+        <Space wrap className='center-button-div'> 
         <Link to={"events/" + event._id} key={event._id}>
   
         <Button className="btn-leer">Leer más</Button>
