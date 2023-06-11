ca1b7e1fbe7c6655bd6ee6cebcecc637e0b99176
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-06-11 19:31:24 +0200

    Changes in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
     };
 
     return (
-<<<<<<< HEAD
         
             <nav>
                 <div><img className="imag-header" src={Logo} alt="Marina de empresas" /></div>
@@ -24,15 +23,6 @@ const Header = () => {
                 </div>
             </nav>
        
-=======
-        <nav>
-            <img className="imag-header" src={Logo} alt="Marina de empresas" />
-            <button className='user-img' onClick={handleUserClick}>
-                {userData ? <img src={`http://localhost:8080/images/user/${userData.image}`} alt="User" /> : <></>}
-            </button>
-
-        </nav>
->>>>>>> 660b3c679b8d88e9fd3e4941530d701c1c787779
     );
 };
 export default Header;
