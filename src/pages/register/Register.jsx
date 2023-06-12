0af12a78048020ee057173dc6138879d039790ad
Jose Llanas <josellanasmota86@gmail.com>
2023-06-12 17:29:26 +0200

    legal terms word of register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -176,7 +176,7 @@ const Register = () => {
           <label>Repetir contraseña</label>
           <Input.Password className="inputPassword" type="password" name="confirmPassword" value={confirmPassword}placeholder="Contraseña" onChange={onChange}/>
         </div>
-        <Checkbox onChange={onChange}>Checkbox</Checkbox>
+        <Checkbox onChange={onChange}>Terminos de privacidad</Checkbox>
           {errorMessage && (
           <div className="div-error">
             {errorMessage.split('\n').map((error, index) => (
