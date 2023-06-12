ad63b78563bbbeba4707fae97cc2b4a6212c88ad
kbastamow <bastamowkat@gmail.com>
2023-06-12 12:39:14 +0200

    Headers en Axios

--- src/features/users/userService.js
+++ src/features/users/userService.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080";
+const token = JSON.parse(localStorage.getItem("token")) || "";
 
 const getAll = async() => {
     try {
-        const res = await axios.get(API_URL + "/users/getAll")
+        const res = await axios.get(API_URL + "/users/getAll", {
+            headers: {
+                'Authorization': token,
+              } })
         console.log(res.data)
         return(res.data) 
     } catch (error) {
@@ -14,7 +18,10 @@ const getAll = async() => {
 }
 
 const getById = async (_id) => {
-    const res = await axios.get(API_URL + "/users/getById/" + _id);
+    const res = await axios.get(API_URL + "/users/getById/" + _id, {
+        headers: {
+            'Authorization': token,
+          } });
     return res.data;
   };
 
