b940003dce2b21a60ab72aef2baa4a6e26238fcb
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-06-12 17:07:17 +0200

    Solve errors in pages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,25 @@
-import "./Header.scss"
+import "./Header.scss";
 import Logo from "../../assets/logo.png";
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FiBell } from "react-icons/fi";
-
 
 const Header = () => {
     const userData = JSON.parse(localStorage.getItem('user'));
-    const navigate = useNavigate();
-    const handleUserClick = () => {
-        navigate(`./userProfile/${userData._id}`)
-    };
 
     return (
-        
-            <nav>
-                <div><img className="imag-header" src={Logo} alt="Marina de empresas" /></div>
-                <div>
-                    <FiBell/>
-                    <button className='user-img' onClick={handleUserClick}>
-                        <img src={`http://localhost:8080/images/user/${userData.image}`} alt="User" />
-                    </button>
-                </div>
-            </nav>
-       
+        <nav>
+            <div>
+                <img className="imag-header" src={Logo} alt="Marina de empresas" />
+            </div>
+            <div>
+                <FiBell />
+                <Link to={`/userProfile/${userData._id}`} className='user-img'>
+                    <img src={`http://localhost:8080/images/user/${userData.image}`} alt="User" />
+                </Link>
+            </div>
+        </nav>
     );
 };
+
 export default Header;
+
