ab456b25a2d8a1b430d7fbe6ab0f75323e32d720
kbastamow <bastamowkat@gmail.com>
2023-06-13 14:37:43 +0200

    Events toggle in progress

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,10 +8,25 @@ import { Button, Menu  } from 'antd';
 import EventCard from "../../components/eventCard/eventCard";
 import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
-
+import { Switch } from 'antd';
 export const Home = () => {
       
 const dispatch = useDispatch()
+
+
+
+const onChange = (checked) => {
+  console.log(`switch to ${checked}`);
+
+  if (!checked) {
+    console.log("Filtra por fecha")
+  }
+
+  if (checked) {
+    console.log("Filtra por recomendacion")
+  }
+};
+
 
 useEffect(() => {
   dispatch(getAll())
@@ -27,6 +42,8 @@ useEffect(() => {
       <span  className="blue-menu-btn blue-menu-active">Eventos</span>
       <span className="blue-menu-btn">Noticias</span>
       </div>
+      <div className="event-toggle"><span>Por fecha</span><span><Switch defaultUnchecked onChange={onChange} /></span><span>Recomendados</span></div>
+
     </div>
     <EventCard></EventCard>
     <NavBar/>
