c28fcb4d68b8ff96ff2c942210296379b8bf2b91
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-06-13 11:06:24 +0200

    add token to enterprisesService

--- src/features/enterprises/enterprisesService.js
+++ src/features/enterprises/enterprisesService.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 const API_URL = "http://localhost:8080";
+const token = JSON.parse(localStorage.getItem("token")) || "";
+
 
 const getAll = async () => {
     try {
-        const res = await axios.get(API_URL + "/lanzadera/getAll")
+        const res = await axios.get(API_URL + "/lanzadera/getAll",{
+            headers: {
+                'Authorization': token,
+              } })
         console.log(res.data)
         return(res.data) 
     } catch (error) {
