c208bb01336978f7ec52dd39ef4957f593c59d44
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-06-13 16:48:40 +0200

    Chat updates styles

--- src/components/chatCard/ChatCard.jsx
+++ src/components/chatCard/ChatCard.jsx
@@ -1,48 +1,58 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import DateTimeConverter from '../dateTimeConverter/DateTimeConverter'
-import { Link } from 'react-router-dom'
-import "./ChatCard.scss"
-import { BsArrowUpRightCircle } from 'react-icons/bs'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import DateTimeConverter from "../dateTimeConverter/DateTimeConverter";
+import { Link } from "react-router-dom";
+import "./ChatCard.scss";
+import { BsArrowUpRightCircle } from "react-icons/bs";
 
 const ChatCard = () => {
+  const imagePath = "http://localhost:8080/images/user/";
+  const { myChats } = useSelector((state) => state.chat);
 
-const imagePath = "http://localhost:8080/images/user/"
-const {myChats} = useSelector(state => state.chat)
-
-if (myChats.length < 1) return <>No hay chats</>
-console.log(myChats)
+  if (myChats.length < 1) return <>No hay chats</>;
+  console.log(myChats);
 
   return (
     <>
-    {myChats.map(chat => (
-        <Link to={`/chat/instant/${chat._id}`} style={{ textDecoration: 'none' }} >
-        <div key={chat._id} className="chatcontacts-card">
+      {myChats.map((chat) => (
+        <Link
+          to={`/chat/instant/${chat._id}`}
+          style={{ textDecoration: "none" }}
+        >
+          <div key={chat._id} className="chatcontacts-card">
             <div className="contact-img-container">
-                <img src={imagePath + chat.userIds[0].image} alt="" />
+              <img src={imagePath + chat.userIds[0].image} alt="" />
             </div>
 
             <div className="contact-details">
-               <p>{chat.userIds[0].name} {chat.userIds[0].surname}</p>
-               <div className="last-msg">{chat.lastMsg ? <>  <span>{chat.lastMsg.sender}:</span> {chat.lastMsg.content.slice(0, 35)} </> : <></>}</div>
-               <div className="last-msg-time"><DateTimeConverter datetime={chat.updatedAt}/></div>
+              <div>
+                <p>
+                  {chat.userIds[0].name} {chat.userIds[0].surname}
+                </p>
+                <div className="last-msg">
+                  {chat.lastMsg ? (
+                    <>
+                      <span>{chat.lastMsg.sender}:</span>{" "}
+                      {chat.lastMsg.content.length > 35
+                        ? chat.lastMsg.content.slice(0, 35) + "..."
+                        : chat.lastMsg.content}
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </div>{" "}
+              </div>
+              <div>
+                <div className="last-msg-time">
+                  <DateTimeConverter datetime={chat.updatedAt} />
+                </div>
+              </div>
             </div>
-            
-            <div className="contact-right">
-            <BsArrowUpRightCircle></BsArrowUpRightCircle>
-            </div>
-     
-
-        </div>
+          </div>
         </Link>
+      ))}
+    </>
+  );
+};
 
-
-
-    ))}
-
-</>
-  )
-}
-
-export default ChatCard
+export default ChatCard;
