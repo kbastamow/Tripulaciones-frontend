761af115b476969bea174e38ec070d63d23c4b48
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-06-14 11:49:26 +0200

    added filters enterprises

--- src/pages/enterprises/Enterprises.jsx
+++ src/pages/enterprises/Enterprises.jsx
@@ -12,6 +12,7 @@ const Enterprises = () => {
   const [limit, setLimit] = useState(16);
   const [filter, setFilter] = useState('');
   const [selectedTopic, setSelectedTopic] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
 
 
 
@@ -22,17 +23,22 @@ const Enterprises = () => {
   }
 
   useEffect(() => {
-    dispatch(getAll(limit, filter));
+    dispatch(getAll(limit, filter, selectedDate));
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [limit, filter]);
+  }, [limit, filter, selectedDate]);
 
   const handleFilter = (category) => {
     setFilter(category);
     setSelectedTopic('');
     setLimit(16);
+  };
+
+  const handleDateFilter = (date) => {
+    setFilter(date);
+    setSelectedDate('');
   };
 
   const enterprise = (enterprises.length > 0)
@@ -43,11 +49,16 @@ const Enterprises = () => {
         }
         return enterprise.topics.includes(selectedTopic);
       })
+      .filter((enterprise) => {
+        if (selectedDate === '') {
+          return true;
+        }
+        return enterprise.updatedAt.slice(0, 10).includes(selectedDate) ;
+      })
       .slice(0, limit)
       .map((enterprise) => (
         <div key={enterprise._id} className="enterprises-list">
           <div className='enterprise-imag'><img src={enterprise.logo} alt="logo" /></div>
-          {/* <p className='enterprise-name'>{enterprise.name} :</p> */}
           <p className='enterprise-description'>{enterprise.description.length > 60 ? enterprise.description.slice(0, 60) + '...' : enterprise.description}</p>
           <p className='topics-container'>
             {enterprise.topics.slice(0, 2).map((topic) => (
@@ -63,6 +74,47 @@ const Enterprises = () => {
       ))
     : [];
 
+    const topics = [...new Set(enterprises.flatMap((enterprise) => enterprise.topics))];
+
+    const topicFilter = topics.length > 0 ? (
+      <select
+        className="topic-filter"
+        value={selectedTopic}
+        onChange={(event) => setSelectedTopic(event.target.value)}
+      >
+        <option value="">Filtrar por categoría</option>
+        {topics.map((topic) => (
+          <option
+            key={topic}
+            value={topic}
+          >
+            {topic}
+          </option>
+        ))}
+      </select>
+    ) : null;
+
+    const dates = [...new Set(enterprises.map((enterprise) => enterprise.updatedAt))];
+          console.log(dates)
+    const dateFilter = dates.length > 0 ? (
+      <select
+        className="date-filter"
+        value={selectedDate}
+        onChange={(event) => handleDateFilter(event.target.value)}
+      >
+        <option value="">Filter by date</option>
+        {dates.sort((a, b) => new Date(b) - new Date(a)).map((date) => (
+          <option
+           key={date}
+            value={date}
+            >
+              {date.slice(0, 10)}
+              </option>
+        ))}
+      </select>
+    ) : null;
+    
+
   return (
     <div>
       <div className="sticky">
@@ -73,25 +125,14 @@ const Enterprises = () => {
           <span className={`blue-menu-btn${filter === 'Angels' ? ' blue-menu-active' : ''}`} onClick={() => handleFilter('Angels')}>Angels</span>
         </div>
         <div className="enterprise-h2"><h2>Empresas que coinciden con tus intereses :</h2></div>
-      
-      <select
-        className="topic-filter"
-        value={selectedTopic}
-        onChange={(event) => setSelectedTopic(event.target.value)}
-      >
-        <option value="">Filtrar por categoria</option>
-        {enterprises.map((enterprise) => (
-          enterprise.topics.map((topic, index) => (
-            <option
-              key={`${enterprise.id}-${index}`}
-              value={topic}
-              
-            >
-              {topic}
-            </option>
-          ))
-        ))}
-      </select>
+        <div className="filter-div">
+        <div>
+          {topicFilter}
+        </div>
+        <div className="date-filter-div">
+          {dateFilter}
+        </div>
+        </div>
       </div>
       <div className="enterprise-general-container">
         <div className="enterprise-container">
